4c2b1caaa57b1c0e774afcb72eb91362f5927df4
yhkim <kvoyant@gmail.com>
2019-04-28 14:31:31 +0900

    권한 설정 라이브러리 사용 예시  - Manifest에 원하는 권한을 모두 적자  - 마쉬맬로우 이후의 대응으로 TedPermission 라이브러리에서 다시 확인받자

--- app/src/main/java/com/tjeit/librarypractics/MainActivity.java
+++ app/src/main/java/com/tjeit/librarypractics/MainActivity.java
@@ -1,11 +1,20 @@
 package com.tjeit.librarypractics;
 
+import android.Manifest;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
 import com.tjeit.librarypractics.databinding.ActivityMainBinding;
+
+import java.util.List;
 
 public class MainActivity extends BaseActivity {
 
@@ -21,7 +30,37 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+        act.callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                PermissionListener permissionlistener = new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted() {
+//                        Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "권한 승인", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(List<String> deniedPermissions) {
+//                        Toast.makeText(MainActivity.this, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "권한 불가\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                };
+
+                TedPermission.with(mContext)
+                        .setPermissionListener(permissionlistener)
+//                        .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+                        .setDeniedMessage("전화 권한을 거부하면 통화가 불가합니다.\n\nPlease turn on permissions at [Setting] > [Permission]")
+//                        .setPermissions(Manifest.permission.READ_CONTACTS, Manifest.permission.ACCESS_FINE_LOCATION)
+                        .setPermissions(Manifest.permission.CALL_PHONE)
+                        .check();
+
+                Uri uri = Uri.parse(String.format("tel:%s", act.phoneNumberTxt.getText().toString()));
+                Intent intent = new Intent(Intent.ACTION_CALL, uri);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
